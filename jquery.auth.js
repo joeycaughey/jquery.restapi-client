d2dfd791b641cd28ff87dbe0f74aa2da4af859ee
Joey Caughey <joey.caughey@gmail.com>
2015-06-04 23:34:06 -0400

    Update jquery.auth.js

--- jquery.auth.js
+++ jquery.auth.js
@@ -1,89 +1,83 @@
 var Auth = {
-	user: false,
-	init: function() {
-		var self = this;
-		self.user = (jQuery.jStorage.get('user')) ? 
-			jQuery.jStorage.get('user') : false;
+    user: false,
+    restrict: function(group) {
+        var self = this
+        var groups = group.split(",");
+        var disable = true;
 
-		self.check();
+        user = $.jStorage.get('user');
+        user = (user) ? user : false;
 
-		console.log("Initalizing Authentication...");
-	},
-	restrict: function(group) {
-		var self = this
-		if (!self.user || !self.user.groups.contains(group)) {
-			self.logout();
-		} 
-		return true;
-	},
-	check: function() {
-		var self = this;
-		self.user = (jQuery.jStorage.get('user')) ? 
-			jQuery.jStorage.get('user') : false;
+        if (user && (typeof user.groups === 'object')) {
+            $.each(groups, function(i, group) {
+                if (user.groups.contains(group)) {
+                    disable = false;
+                }
+            });
+        }
+       
+        if (disable) {
+            self.logout();
+        }
+        
+        return true;
+    },
+    check: function() {
+        var self = this;
+      
+        user = $.jStorage.get('user');
+        user = (user) ? user : false;
 
+        if (user) {
+            $("[data-authenticate-group]").each(function() {
+                var groups = $(this).data("authenticate-group").split(",");
+                var disable = true;
+            
+                if (user && (typeof user.groups === 'object')) {
+                    $.each(groups, function(i, group) {
+                        if (user.groups.contains(group)) {
+                            disable = false;
+                        }
+                    })
+                }
 
-		$("[data-authenticate-group]").each(function(){
-			var groups = $(this).data("authenticate-group").split(",");
-			var disable = false;
+                if (disable) $(this).remove();  
+            });
+            $("[data-authenticated]").show().css("visibility", "visible");
+            $("[data-authenticated-hide]").hide().css("display", "none");
+        }
+        $("[data-show-loged-in]").each(function() {
+            var value = $(this).data("authenticate-logedin");
+            if (user) {
+                if (value == "hide") {
+                    $(this).hide();
+                } else {
+                    $(this).show();
+                }
+            }
+        });
 
-			if (self.user) {
-				$.each(groups, function(i, group){
-					if (!self.user.groups.contains(group)) {
-						disable = true;
-					}
-				});
-			} else {
-				if ($(this).data("authenticate-group")=="logout") {
-					disable = false;
-				} else {
-					disable = true;
-				}
-			}
-
-			if (disable) $(this).remove();
-
-			//console.log("GROUPS", self.user.groups, groups, disable);
-
-		});
-
-		$("[data-is-loged-in]").each(function(){
-			var value = $(this).data("authenticate-logedin");
-
-			if (self.user) {
-				if (value=="hide") {
-					$(this).hide();
-				} else {
-					$(this).show();
-				}
-			}
-		});
-
-		console.log("Checking Authentication...");
-	},
-	login: function(email, password, facebook_user_id) {
-		Loading.show();
-	
-		API.get('ModuleAPITokensModel/login/', {
-	            email: email,
-	            password: password,
-	            facebook_user_id: facebook_user_id
-	         }, function(request){
-	            if (request.success) {
-	                jQuery.jStorage.deleteKey("user");                  
-	                jQuery.jStorage.set("user", request.response);
-	
-	            	document.location = request.response.login_url;
-	            	location.reload(true);
-	            } else {
-	               $("#errors").text(request.response);
-	            }
-	            Loading.hide();
-	        });
-	},
-	logout: function() {
-		jQuery.jStorage.deleteKey("user");   
-        	document.location = '/';
-	}
+        console.log("Checking Authentication...");
+    },
+    login: function(email, password, facebook_user_id) {
+        API.get('ModuleAPITokensModel/login/', {
+            email: email,
+            password: password,
+            facebook_user_id: facebook_user_id
+        }, function(request) {
+            if (request.success) {
+                $.jStorage.deleteKey("user");
+                $.jStorage.set("user", request.response);
+                document.location = request.response.login_url;
+                location.reload();
+            } else {
+                $("#errors").text(request.response);
+            }
+        });
+    },
+    logout: function() {
+        $.jStorage.deleteKey("user");
+        document.location = '/';
+    }
 }
 console.log("Loaded API Authentication.");
-
