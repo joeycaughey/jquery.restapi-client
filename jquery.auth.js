26f5f273e47beae4cdaa42ad08585524e2b79c3d
Joey Caughey <joey.caughey@gmail.com>
2015-07-28 11:52:49 -0400

    Update jquery.auth.js

--- jquery.auth.js
+++ jquery.auth.js
@@ -1,47 +1,80 @@
 var Auth = {
     user: false,
-    restrict: function(group) {
+    restrict: function(groups, callback) {
         var self = this
-        var groups = group.split(",");
         var disable = true;
 
-        user = $.jStorage.get('user');
-        user = (user) ? user : false;
+        Auth.check();
+
+        if (typeof(Storage) !== "undefined") {
+            if (localStorage.getItem("user") !== "undefined") {
+                // Code for localStorage/sessionStorage.
+                user = JSON.parse(localStorage.getItem("user"));
+            } else {
+                user = false;
+            }
+            self.user = user;
+        } else {
+            // Sorry! No Web Storage support..
+            alert("No local storage");
+        }
 
         if (user && (typeof user.groups === 'object')) {
             $.each(groups, function(i, group) {
-                if (user.groups.contains(group)) {
+                if (user.groups.indexOf(group) >= 0) {
                     disable = false;
                 }
             });
         }
-       
+
         if (disable) {
             self.logout();
         }
-        
+       
+        setTimeout(function(){  callback(); }, 500)
+
         return true;
     },
     check: function() {
         var self = this;
-      
-        user = $.jStorage.get('user');
-        user = (user) ? user : false;
 
-        if (user) {
+        if (typeof(Storage) !== "undefined") {
+            // Code for localStorage/sessionStorage.
+
+            if (localStorage.getItem("user") !== "undefined") {
+                // Code for localStorage/sessionStorage.
+                user = JSON.parse(localStorage.getItem("user"));
+            } else {
+                user = false;
+            }
+            self.user = user;
+        } else {
+            // Sorry! No Web Storage support..
+            alert("No local storage");
+        }
+
+        if (user && user != "false") {
+            API.disable_loader = true;
+            API.get('ModuleAPITokensModel/check/', {}, function(request) {
+                if ( (request.success && !request.response)) { // !request.success || 
+                    self.logout();   
+                }
+            });
+            API.disable_loader = false;
+
             $("[data-authenticate-group]").each(function() {
                 var groups = $(this).data("authenticate-group").split(",");
                 var disable = true;
-            
+
                 if (user && (typeof user.groups === 'object')) {
                     $.each(groups, function(i, group) {
-                        if (user.groups.contains(group)) {
+                        if (user.groups.indexOf(group) !== -1) {
                             disable = false;
                         }
                     })
                 }
 
-                if (disable) $(this).remove();  
+                if (disable) $(this).remove();
             });
             $("[data-authenticated]").show().css("visibility", "visible");
             $("[data-authenticated-hide]").hide().css("display", "none");
@@ -57,26 +90,31 @@ var Auth = {
             }
         });
 
-        console.log("Checking Authentication...");
+        //console.log("Checking Authentication...");
     },
-    login: function(email, password, facebook_user_id) {
+    login: function(email, password, options) {
+        var self = this;
+        
+        localStorage.removeItem("user");
+
         API.get('ModuleAPITokensModel/login/', {
             email: email,
             password: password,
-            facebook_user_id: facebook_user_id
+            options: options
         }, function(request) {
             if (request.success) {
-                $.jStorage.deleteKey("user");
-                $.jStorage.set("user", request.response);
+                self.user = request.response;
+                localStorage.setItem("user", JSON.stringify(self.user));
                 document.location = request.response.login_url;
-                location.reload();
+                Popup.close();
+                self.check();
             } else {
-                $("#errors").text(request.response);
+                $("#login-form").find("div.errors").html(request.response).show();
             }
         });
     },
     logout: function() {
-        $.jStorage.deleteKey("user");
+        localStorage.removeItem("user");
         document.location = '/';
     }
 }
